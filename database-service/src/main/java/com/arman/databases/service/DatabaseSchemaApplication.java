4587f65abdccd0bf6cb1f80880e44a56140d8cc4
ArmanKamal <armankamal2421@gmail.com>
2023-09-25 23:10:20 -0500

    Added DB Service

---
+++ database-service/src/main/java/com/arman/databases/service/DatabaseSchemaApplication.java
@@ -0,0 +1,13 @@
+package com.arman.databases.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseSchemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseSchemaApplication.class, args);
+	}
+
+}
---
+++ database-service/src/test/java/com/arman/databases/service/DatabaseSchemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arman.databases.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseSchemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
